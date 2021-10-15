fd60ff10c4c913106cd1b02a2b2239d447a10681
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-10-15 14:17:17 +0300

    get data from api DB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import GlobalStyle from "./components/GlobalStyle";
-
+//Api
+import {currentData} from "./api";
+import axios from "axios";
+ 
 function App() {
+
+  const [weather,setWeather] = useState(null);
+
+  useEffect(()=>{
+    axios.get(currentData("Rison Le Zion")).then((data)=>{
+      setWeather(data.data);
+    })
+  },[]);
+  
   return (
     <div className="App">
       <GlobalStyle/>
       <h1>Weather App</h1>
+      {weather && (
+        <div>
+          <h2>{weather.location.country}</h2>
+          <h2>{weather.location.name}</h2>
+        </div>
+      )}
     </div>
   );
 }
