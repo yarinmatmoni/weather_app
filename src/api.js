73759a670383f50cf20efbc0124e3bdceaa29a01
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-10-18 15:09:40 +0300

    Get forecast data

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 
 const baseUrl = "http://api.weatherapi.com/v1";
+const numDays = 1;
 
 //Key and KeyURL
 const key = process.env.REACT_APP_WEATHER_API;
 const KeyURL = `?key=${key}`;
 
-//Current Data 
-export const currentData = (location_name) => `${baseUrl}/current.json${KeyURL}&q=${location_name}`;
+//Current Data and Forecast
+export const currentData = (location_name) => `${baseUrl}/forecast.json${KeyURL}&q=${location_name}&days=${numDays}&aqi=no&alerts=no`;
