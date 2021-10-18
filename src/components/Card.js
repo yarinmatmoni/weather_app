55b5cfcee08df189a79e8a6b5398643ef74eda9f
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-10-18 13:25:46 +0300

    Add details

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,9 +7,25 @@ function Card({searchResult,locationName}) {
         <>
             {locationName ? (
                 <StyleCard>
-                    <NameLocation>{searchResult.location.country}</NameLocation>
-                    <p>{searchResult.location.region}</p>
-                    <p>blbblblblbl</p>
+                    <NameLocation>{searchResult.location.name}</NameLocation>
+                    <h3>{`${searchResult.location.country}, ${searchResult.location.region}`}</h3>
+                    <Details>
+                        <Info>
+                            <h3 className="title">Current:</h3>
+                            <h3 className="answer">{`${searchResult.current.temp_c} C`}</h3>
+                            <h3 className="title">Feels Like:</h3>
+                            <h3 className="answer">{`${searchResult.current.feelslike_c} C`}</h3>
+                        </Info>
+                        <Info>
+                            <h3 className="title">Wind:</h3>
+                            <h3 className="answer">{`${searchResult.current.wind_kph} KPH`}</h3>
+                            <h3 className="title">UV:</h3>
+                            <h3 className="answer">{searchResult.current.uv}</h3>
+                        </Info>    
+                        <Info>
+                            <img src={searchResult.current.condition.icon} alt={searchResult.current.condition.text}></img>
+                        </Info>
+                    </Details>
                 </StyleCard>
             ): ""}
         </>
@@ -30,8 +46,38 @@ const NameLocation = styled.div`
     color: dimgray;
     font-family: 'Source Sans Pro', sans-serif;
     font-size: 1.8rem;
-    padding-bottom: 4rem;
+    padding-bottom: 0.2rem;
     padding-top: 0.3rem;
+    font-weight: bold;
+`;
+
+const Details = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 1rem 0rem 1rem 0rem;
+
+    .title{
+        font-size: 1.6rem;
+        font-weight: 100;
+    }
+
+    .answer{
+        padding-bottom: 1rem;
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+`;
+
+const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.3rem;
+
+    img{
+        height: 8rem;
+        width: 100%;
+    }
 `;
 
 export default Card;
