fbb8266f4989bcc68d1eef13e785917b8ecc3a0a
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-22 01:21:10 +0200

    baseUrl

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-
-const baseUrl = "http://api.weatherapi.com/v1";
+const baseUrl = 'https://api.weatherapi.com/v1';
 const numDays = 2;
 
 //Key and KeyURL
@@ -7,4 +6,5 @@ const key = process.env.REACT_APP_WEATHER_API;
 const KeyURL = `?key=${key}`;
 
 //Current Data and Forecast
-export const currentData = (location_name) => `${baseUrl}/forecast.json${KeyURL}&q=${location_name}&days=${numDays}&aqi=no&alerts=no`;
+export const currentData = (location_name) =>
+	`${baseUrl}/forecast.json${KeyURL}&q=${location_name}&days=${numDays}&aqi=no&alerts=no`;
